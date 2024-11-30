e5f1c8eaef587456bd26af80c94d0e7c0c9575ee
Plasma1504 <pusam.tobi@gmail.com>
2024-11-30 14:48:48 +0100

    Run left animation
    fixxed it, now he can run to the left with animation :D

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -31,16 +31,19 @@ func SetDirection() -> bool:
 	if direction == Vector2.ZERO:
 		return false
 		
-	if direction.y == 0:
-		new_dir = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
-	elif direction.x ==0:
-		new_dir = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	if direction.x < 0:
+		new_dir = Vector2.LEFT
+	elif direction.x > 0:
+		new_dir = Vector2.RIGHT
+	elif direction.y < 0:
+		new_dir = Vector2.UP
+	elif direction.y > 0:
+		new_dir = Vector2.DOWN
 	
 	if new_dir == cardinal_direction:
 		return false
 	
 	cardinal_direction = new_dir
-	sprite.scale.x = -1 if cardinal_direction ==Vector2.LEFT else 1
 	return true
 
 
